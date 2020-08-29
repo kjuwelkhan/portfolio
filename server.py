38984f44f6ff4f8a6867762c9e8d28627e2829f4
mirza-khan <68930580+mirza-khan@users.noreply.github.com>
2020-08-29 21:06:55 +0500

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,16 @@ import socket
 import threading
 from pynput.mouse import Button, Controller
 #import ctypes
-import pyautogui
-import sys
 import time
+import sys
 
 #ctypes.windll.shcore.SetProcessDpiAwareness(2)
-
-header = 8
+execute = True
+header = 16
 format = "utf-8"
 port = 3255
 mouse  = Controller()
-execute = True
+
 
 try: 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,7 +19,7 @@ except socket.error:
     print(f"\nError thrown is {socket.error} (socket creation)")
 
 try: 
-    serverip = sock.gethostbyname(socket.gethostname())
+    serverip = #your desired ip here
 except socket.gaierror():
     print(f"\nError thrown is {socket.gaierror()} (GAI)")
     sys.exit()
@@ -43,28 +42,27 @@ def handle_client(conn, addr):
                 button = targets[2]
 
                 if button == "left": button = Button.left
-                elif button == "right": 
-                    button = Button.right
-                    break
+                elif button == "right": button = Button.right
+                print(targets[3])
+                pressed = targets[3]
+                if pressed == "False": pressed = False
+                else: pressed = True
 
-                pressed = bool(targets[3])
-                
-                if pressed == True: 
-                    mouse.press(button)
-                    execute == False
-                else:
-                    mouse.release(button)
-                    execute == True
-
+                print("Button is:", button)
+                print("Pressed value is: ", pressed)
                 mouse.position = (x,y)
+                if pressed == True:
+                    mouse.press(button)
+
+                elif pressed == False:
+                    mouse.release(button)
                 
 
-            elif (len(targets) == 2 and execute == True):
+            elif (len(targets) == 2):
                 mouse.position = (x,y)
             else: 
                 raise Exception("Non valid input given")
 
-            
             if (message == "disconnect"):
                 break
         
