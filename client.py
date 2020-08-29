2e9bbb6533403274c913b0de12a3eb5b61799f1c
mirza-khan <68930580+mirza-khan@users.noreply.github.com>
2020-08-29 18:37:09 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
 import time
 from pynput import *
-
+import sys
 
 header = 8
 format = "utf-8"
 port = 3255
-server = input("After running server.py please type the python ip here: ")
+server = str(input("After running server.py please type the python ip here: "))
 
 
 clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,12 +22,20 @@ def send(msg):
 
 #Track cursor definitions
 def track_cursor(x, y):
-    send((x,y))
+    send(str(x) + " " + str(y))
 
 def on_click(x, y, button, pressed):
     #send("{0} at {1}".format('Pressed' if pressed else 'Released', (x,y)))
-    send(((x, y), (button, pressed)))
-
+    print(str(button))
+    print(type(button))
+    if str(button) == "Button.left":
+        button = "left"
+    if str(button) == "Button.right":
+        button = "right"
+        sys.exit()
+    send(str(x) + " " + str(y) + " " + button + " " + str(pressed))
+    time.sleep(1)
+    
 with mouse.Listener(on_move = track_cursor,on_click = on_click) as hlhook:
     hlhook.join()
 
