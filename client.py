00f332cc8b0447f4e2d88bc13d6bae816e41bde4
mirza-khan <68930580+mirza-khan@users.noreply.github.com>
2020-08-30 08:15:51 +0500

    Update client.py

--- client.py
+++ client.py
@@ -2,11 +2,14 @@ import socket
 import time
 from pynput import *
 import sys
+import pyautogui
 
 header = 16
 format = "utf-8"
 port = 3255
-server = #your ip where you hosted the server
+server = #your chosen ip here
+clientSysWidth = str(pyautogui.size()[0])
+clientSysHeight = str(pyautogui.size()[1])
 
 clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsock.connect((server, port))
@@ -34,6 +37,8 @@ def on_click(x, y, button, pressed):
         button = "left"
     
     send(str(x) + " " + str(y) + " " + button + " " + str(pressed))
+
+send(clientSysWidth + " " + clientSysHeight + " <- Here Is The System Resolution")
 
 with mouse.Listener(on_move = track_cursor,on_click = on_click) as hlhook:
     hlhook.join()
