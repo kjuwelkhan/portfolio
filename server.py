de9ae3535b2903612db76f3c837c7c54516bbbda
mirza-khan <68930580+mirza-khan@users.noreply.github.com>
2020-08-30 08:16:43 +0500

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from pynput.mouse import Button, Controller
 #import ctypes
 import time
 import sys
+import pyautogui
 
 #ctypes.windll.shcore.SetProcessDpiAwareness(2)
 execute = True
@@ -12,6 +13,14 @@ format = "utf-8"
 port = 3255
 mouse  = Controller()
 
+hostSysWidth = pyautogui.size()[0]
+hostSysheight = pyautogui.size()[1]
+
+ClientSysWidth = None
+ClientSysheight = None
+
+ratioHeight = 1
+ratioWidth = 1
 
 try: 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,7 +28,7 @@ except socket.error:
     print(f"\nError thrown is {socket.error} (socket creation)")
 
 try: 
-    serverip = #your chosen ip
+    serverip = #your chosen ip here
 except socket.gaierror():
     print(f"\nError thrown is {socket.gaierror()} (GAI)")
     sys.exit()
@@ -35,31 +44,40 @@ def handle_client(conn, addr):
             message_length = int(message_length)
             message = conn.recv(message_length).decode(format)
             targets = message.split(' ')
-            print(targets)
+            #print(targets)
+            #print(len(targets))
             x = int(targets[0])
             y = int(targets[1])
-            if (len(targets) == 4):
+            if (len(targets) == 8):
+                clientSysWidth = int(targets[0])
+                clientSysHeight = int(targets[1])
+
+                ratioHeight = hostSysheight/clientSysHeight
+                ratioWidth = hostSysWidth/clientSysWidth
+
+            elif (len(targets) == 4):
                 button = targets[2]
 
                 if button == "left": button = Button.left
                 elif button == "right": button = Button.right
-                print(targets[3])
+                #print(targets[3])
                 pressed = targets[3]
                 if pressed == "False": pressed = False
                 else: pressed = True
 
-                print("Button is:", button)
-                print("Pressed value is: ", pressed)
-                mouse.position = (x,y)
+                #print("Button is:", button)
+                #print("Pressed value is: ", pressed)
+                mouse.position = (x * ratioWidth,y * ratioHeight)
                 if pressed == True:
                     mouse.press(button)
 
                 elif pressed == False:
                     mouse.release(button)
                 
-
             elif (len(targets) == 2):
-                mouse.position = (x,y)
+                mouse.position = (x * ratioWidth,y * ratioHeight)
+            
+
             else: 
                 raise Exception("Non valid input given")
 
